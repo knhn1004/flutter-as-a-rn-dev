8f5b28db516b73a9e6fa4e783347acb49b1ed61d
oliver <kunhan945@gmail.com>
2022-05-11 15:09:55 +0800

    displaying categories

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_as_a_rn_dev/models/category.dart';
 
 class HomeScreen extends StatelessWidget {
+  final List<Category> categories = [
+    Category(id: 'today', name: 'Today'),
+    Category(id: 'scheduled', name: 'Scheduled'),
+    Category(id: 'all', name: 'All'),
+    Category(id: 'flagged', name: 'Flagged'),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,34 +28,38 @@ class HomeScreen extends StatelessWidget {
           children: [
             Expanded(
                 child: GridView.count(
-              crossAxisCount: 2,
-              childAspectRatio: 16 / 9,
-              crossAxisSpacing: 10,
-              mainAxisSpacing: 10,
-              padding: EdgeInsets.all(10),
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: Color(0xFF1A191D)),
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: Color(0xFF1A191D)),
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: Color(0xFF1A191D)),
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: Color(0xFF1A191D)),
-                ),
-              ],
-            )),
+                    crossAxisCount: 2,
+                    childAspectRatio: 16 / 9,
+                    crossAxisSpacing: 10,
+                    mainAxisSpacing: 10,
+                    padding: EdgeInsets.all(10),
+                    children: categories
+                        .map(
+                          (cat) => Container(
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                color: Color(0xFF1A191D)),
+                            child: Padding(
+                                padding: const EdgeInsets.all(10),
+                                child: Column(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceBetween,
+                                        children: [
+                                          Icon(Icons.email),
+                                          Text('0'),
+                                        ],
+                                      ),
+                                      Text(cat.name),
+                                    ])),
+                          ),
+                        )
+                        .toList())),
             Container(
                 padding: EdgeInsets.all(10),
                 child: Row(
